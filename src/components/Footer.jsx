e18f7326b93af7e8d30d141e02a1eb0eef084830
KalpeshHire <kalpesh.hire@technokrate.com>
2024-10-11 19:32:00 +0530

    skill updated footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,7 +3,7 @@ import React from "react";
 function Footer() {
   return (
     <>
-      <div className="scroll-mt-10 w-full flex">
+      <div className="scroll-mt-10 w-full flex mb-2">
         <div className=" flex flex-col container justify-center items-center">
           <h1 className="text-3xl text-red-500 footer-text">Kalpesh Hire</h1>
           <p className="text-xs md:text-sm font-serif">
